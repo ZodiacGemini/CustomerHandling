bdcbb2e1cdeac6b53094ca06e2bb86e55d104ee7
ZodiacGemini <alexanderedvinsson@icloud.com>
2017-04-12 11:42:34 +0200

    Project

---
+++ Web2/Models/DataManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web2.Models
+{
+    public static class DataManager
+    {
+        static List<Customer> customerList = new List<Customer>
+        {
+            new Customer { Id = 1, CompanyName = "Spotify", Address = "Spotifygatan", EmployeeCount = 3000 },
+            new Customer { Id = 2, CompanyName = "Skype", Address = "Skypegatan", EmployeeCount = 10 },
+            new Customer { Id = 3, CompanyName = "Warm Kitten", Address = "PontusLand", EmployeeCount = 1 },
+            new Customer { Id = 4, CompanyName = "Izettle", Address = "Sofiaspartnergatan", EmployeeCount = 2 },
+        };
+
+        public static void AddCustomer(Customer customer)
+        {
+            customer.Id = (customerList.Max(c => c.Id) + 1);
+
+            customerList.Add(customer);
+        }
+
+        static public Customer[] GetAllCustomers() => customerList.ToArray();
+
+       public static void Remove(int id)
+        {
+            customerList.Remove(customerList.SingleOrDefault(c => c.Id == id));
+        }
+    }
+}
